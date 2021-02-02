a01729a8e458cc5cd50182aa922afe2ab00c8bc1
MUZAFFER <MUZAFFER@LAPTOP-6TD3P938>
2021-02-02 17:02:26 +0300

    Ödev

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("\nYeni Müşteri Eklendi:" + "\nVerilen ID:" + musteri.Id + " \nİsim Soyisim:" + musteri.Name + " " + musteri.SurName + " " + " \nTelefon numarası:" + musteri.Number);
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Silinen Müşteri:" + "\n" + musteri.Name + " " + musteri.SurName + "\nHaber ver:" + musteri.Number);
+        }
+
+
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine("Kayıtlı Kişiler:" + "\nID Numarası:" + musteri.Id + "\nAdı Soyadı:" + musteri.Name + " " + musteri.SurName + "\nTelefon Numarası" + musteri.Number);
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Name = "Ali";
+            musteri1.SurName = "Cengiz";
+            musteri1.Number = 205014895;
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Name = "Merve";
+            musteri2.SurName = "Ayşe";
+            musteri2.Number = 205026598;
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Name = "Alparslan";
+            musteri3.SurName = "Toksöz";
+            musteri3.Number = 205041262;
+
+            MusteriManager manager = new MusteriManager();
+            manager.Add(musteri1);
+            manager.Delete(musteri1);
+            manager.List(musteri1);
+
+            Musteri[] musteri = new Musteri[] { musteri1, musteri2 , musteri3};
+            
+            foreach (var cst in musteri)
+            {
+                Console.WriteLine("Registered musteri : " + cst.Id+ " "  + cst.Name +" " + cst.SurName);
+            }
+
+        }
+    }
+}
